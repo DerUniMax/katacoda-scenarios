421ffb7c637822e8b0237d9c0ebeeeaa97ff0dbd
HyEnergy <don.rohan@gmx.de>
2021-10-12 13:15:35 +0200

    env-init

--- JSONinDB/env-init.sh
+++ JSONinDB/env-init.sh
@@ -4,10 +4,3 @@
 
 docker run --name postgresql -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 docker exec -it postgresql bash
-
-psql
-
-CREATE TABLE orders (
-	id serial NOT NULL PRIMARY KEY,
-	info json NOT NULL
-)
